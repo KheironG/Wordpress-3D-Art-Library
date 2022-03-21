e5a8cd671e8361aca62ffc9025c8060b422d050a
kheiron <kheiron.gunnarsson@gmail.com>
2022-03-21 17:35:45 +0100

    added corrent nonce to verify nonce function

--- scripts/script-contact.php
+++ scripts/script-contact.php
@@ -1,13 +1,8 @@
 <?php
-
-    $strip = str_replace('\\', '', $_POST["data"]);
-    $data = json_decode( $strip, false );
-
-    if ( !wp_verify_nonce( $data->nonce, 'contact-nonce') ) {
+    if ( !wp_verify_nonce( $data->nonce, 'frontend-ajax-nonce') ) {
         $error['error'] = 'insecure form submisson.';
         echo json_encode( $error );
-        exit;
-    }
+        exit; }
 
 
     if ( $data->option === 'profile-as-user' ) {
@@ -16,31 +11,27 @@
         $sender_email = $sender->user_email;
 
         $receiver = get_user_by( 'login', sanitize_text_field( $data->receiver ) );
-        $receiver_email = $receiver->user_email;
-    }
+        $receiver_email = $receiver->user_email; }
 
     if ( $data->option === 'profile-as-public' ) {
         $sender_name = sanitize_text_field( $data->name );
         $sender_email = sanitize_email( $data->email );
 
         $receiver = get_user_by( 'login', sanitize_text_field( $data->receiver  ) );
-        $receiver_email = $receiver->user_email;
-    }
+        $receiver_email = $receiver->user_email; }
 
     if ( $data->option === 'admin-as-user' ) {
         $sender = wp_get_current_user();
         $sender_name = $sender->user_login;
         $sender_email = $sender->user_email;
 
-        $receiver_email = get_bloginfo('admin_email');
-    }
+        $receiver_email = get_bloginfo('admin_email'); }
 
     if ( $data->option === 'admin-as-public' ) {
         $sender_name = sanitize_text_field( $data->name );
         $sender_email = sanitize_email( $data->email );
 
-        $receiver_email = get_bloginfo('admin_email');
-    }
+        $receiver_email = get_bloginfo('admin_email'); }
 
     $subject = sanitize_text_field( $data->subject );
     $message = sanitize_textarea_field( $data->message );
@@ -48,14 +39,12 @@
     if ( empty( $sender_email ) || !is_email( $sender_email ) ) {
         $error['email'] = 'valid email required.';
         echo json_encode( $error );
-        exit;
-    }
+        exit; }
 
     if ( empty( $message ) || ctype_space( $message ) ) {
         $error['message'] = 'message required.';
         echo json_encode( $error );
-        exit;
-    }
+        exit; }
 
     $body = '<p>'. $message .'</p>';
     $headers = array('From: '. $sender_name .' <'.$sender_email.'>');
@@ -65,8 +54,7 @@
     if ( $send_email === false ) {
         $error['error'] = 'unable to send email.';
         echo json_encode( $error );
-        exit;
-    }
+        exit; }
 
     echo json_encode( 'success' );
     exit;
